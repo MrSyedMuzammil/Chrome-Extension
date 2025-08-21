77ae761660c855238eae039c2b1a3c9b99d84891
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-08-21 16:19:34 +0500

    Add 'Save Tab' feature and update permissions
    Introduced a 'SAVE TAB' button in the UI and implemented its functionality in script.js to save the current tab's URL. Updated manifest.json to request 'tabs' permission for accessing tab information. Also updated icon.png.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
 const ulEl = document.querySelector("#ul-el");
 const deleteBtn = document.querySelector("#delete-btn");
+const tabBtn = document.querySelector("#tab-btn");
 
 const leadsFromLocalStorage = localStorage.getItem("myLeads");
 
@@ -27,6 +28,14 @@ function saveLead() {
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
   render(myLeads);
 }
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
 
 inputBtn.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
